24ce7073c39d9539ef0103c0506905f531574af4
TurtleOld <shurpo.alexander@gmail.com>
2021-07-21 22:18:31 +0300

    Добавил комментарии

--- settings.py
+++ settings.py
@@ -1,10 +1,12 @@
 import datetime
 
 
+# Выделяем дату из json
 def convert_date(date_time):
     return datetime.datetime.fromtimestamp(date_time).strftime("%Y.%m.%d")
 
 
+# Выделяем время из json
 def convert_time(date_time):
     return datetime.datetime.fromtimestamp(date_time).strftime("%H:%M:%S")
 
